75236d83673b497127d1e3a56d40262d9686cf47
Adarshkumarsingh1212 <adarshkumarsinghmac@gmail.com>
2024-11-25 09:29:18 +0530

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,16 +1,24 @@
 // firebase-init.js
 
-// Firebase Configuration
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Initialize Firebase
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyD9clFYb6g5DIac64ElQ_Yez1l2LKxRWFU",
+    authDomain: "questro01.firebaseapp.com",
+    projectId: "questro01",
+    storageBucket: "questro01.firebasestorage.app",
+    messagingSenderId: "374170494299",
+    appId: "1:374170494299:web:7b48878706bc66124b267e",
+    measurementId: "G-214C2R14ZQ"
+  };
+
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 const storage = firebase.storage();

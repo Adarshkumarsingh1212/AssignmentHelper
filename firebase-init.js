08725c6fbe61434482ab54040c31077bcff2512b
Adarshkumarsingh1212 <adarshkumarsinghmac@gmail.com>
2024-11-25 09:24:51 +0530

    Create firebase-init.js

---
+++ firebase-init.js
@@ -0,0 +1,16 @@
+// firebase-init.js
+
+// Firebase Configuration
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const storage = firebase.storage();
